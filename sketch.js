6057a11790567b29509b495f2976260a24f50a97
Advika77 <70756268+Advika77@users.noreply.github.com>
2020-11-21 11:58:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var polygon,slingShot,circle,Matter,bodies,world,polygon_image,position
+var ground;
+var stand1,stand2;
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(350, 200, 10, 100);
+  const Bodies = World.Bodies;
+}
+
+function draw() {
+  background(255,255,255);  
+
+World.add=(world,polygon);
+
+if(block1.velocityX=3,block2.velocityX=3,block3.velocityX=3,block4.velocityX=3,block5.velocityX=3,block6.velocityX=3,block7.velocityX=3,block8.velocityX=3,block9.velocityX=3,block10.velocityX=3,block11.velocityX=3,block12.velocityX=3){
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+
+
+}
+
+imageMode(CENTER)
+Image(polygon_image,polygon.position.x,polygon.position.y,40,40);
+  drawSprites();
+}
+
+function mouseDragged(){
+
+       Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+
+}
+
+
+function mouseReleased(){
+   slingshot.fly();
+   gameState = "launched";
+}
